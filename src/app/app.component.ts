c8f5b65d20e5169adfb2c09bc928dd31e31abcdf
senorFilip <filipcicfilip1@gmail.com>
2019-11-12 16:03:11 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {UsersService} from './users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
+
+  constructor(private usersService: UsersService) {}
+
+  onSetToInactive(id: number) {
+    this.usersService.onSetToInactive(id);
+  }
+
+  onSetToActive(id: number) {
+    this.usersService.onSetToActive(id);
+  }
+}
